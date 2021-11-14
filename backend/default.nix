f341ed456cf80230d483ddfa3a28809162ca9a02
Philipp Middendorf <pmidden@mailbox.org>
2021-11-14 15:21:33 +0100

    Add GitHub action

--- backend/default.nix
+++ backend/default.nix
@@ -1,2 +1,9 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8104" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./cabal2nix.nix { }
+let nixpkgs = import (builtins.fetchTarball {
+  name = "nixos-unstable-2021-11-20";
+  url = "https://github.com/nixos/nixpkgs/archive/a03719be2e676041847e1b59dafa8ab668fa2d2a.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "1xvcc89m80cpslv84qhbbcyvcjqxhip5drx1arxvaajqv9059nqk";
+    }) {};
+    compiler = "ghc8107";
+in
+  nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./cabal2nix.nix { }
--- backend/shell.nix
+++ backend/shell.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8104" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8107" }:
 (import ./default.nix { inherit nixpkgs compiler; }).env
